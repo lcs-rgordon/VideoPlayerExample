ced055f42ea92f090be44f0cda1d0f5d124e824b
Russell Gordon <github@russellgordon.ca>
2022-03-08 06:26:10 -0500

    Initial Commit

---
+++ VideoPlayerExample/VideoPlayerExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerExampleApp.swift
+//  VideoPlayerExample
+//
+//  Created by Russell Gordon on 2022-03-08.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayerExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
